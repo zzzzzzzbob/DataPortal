93b8ccae83708e5e147ac374d1e11aa064ac9ef9
zzzzzzzbob <zzzzzzzbob@gmail.com>
2020-08-18 23:55:38 +0900

    2020-08-18 mongodb in aws 연결

--- crawler/control_mongo.py
+++ crawler/control_mongo.py
@@ -1,12 +1,14 @@
 import pymongo as pm
+from os.path import dirname, join
 
-FILE = open("db_config", 'r')
-DB_PATH = 
+current_dir = dirname(__file__)
+FILE = open(dirname(__file__)+"/db_config", "r", encoding="utf8")
+DB_PATH = FILE.read()
 
 class to_mongo:
     def __init__(self):
         self.mg = pm.MongoClient(DB_PATH)
-        #self.mg_db = self.mg["DataPortal"]
+        self.mg_db = self.mg["DataPortal"]
         
     def insert_data(self, data, dest):
         self.mg_collection = self.mg_db[dest]
--- crawler/corona_crawler.py
+++ crawler/corona_crawler.py
@@ -7,21 +7,29 @@ from datetime import datetime as dt
 from control_mongo import to_mongo
 # to_mongo.insert_data(data, dest)
 
-data = requests.get("http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=")
-corona_data = pd.read_html(data.content, encoding="utf8")
+DATA_URL = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun="
 
-# 해외유입 환자현황 데이터 세팅
-k = corona_data[0].loc[:,['구분.1', '신규', '누계']]
-data_set = {}
-data_sub_set = {}
-for idx in range(len(k)):
-    # pandas 의 int 형식의 값은 numpy.int 로 몽고에 insert하게되면 타입 에러 발생 --> item() 으로 대체
-    
-    data_sub_set[k.iloc[idx]['구분.1']] = {"신규": k.iloc[idx]['신규'].item(), "누계" : k.loc[idx]['누계'].item()}
-#     print("국가 : {}, 수량 : {}".format(k.iloc[idx]['구분.1'], k.iloc[idx]["신규"]))
-data_set[dt.date(dt.now()).isoformat()] = data_sub_set
+class Corona:
+    def __init__ (self):
+        self.data = requests.get(DATA_URL)
+        self.corona_data = pd.read_html(self.data.content, encoding="utf8")
 
-# print(data_set)
-# Data insert to mongo
-mg = to_mongo()
-mg.insert_data(data_set, 'corona_data')
+    def get_and_insert_data(self):
+        # 해외유입 환자현황 데이터 세팅
+        k = self.corona_data[0].loc[:,['구분.1', '신규', '누계']]
+        data_set = {}
+        data_sub_set = {}
+        for idx in range(len(k)):
+            # pandas 의 int 형식의 값은 numpy.int 로 몽고에 insert하게되면 타입 에러 발생 --> item() 으로 대체            
+            data_sub_set[k.iloc[idx]['구분.1']] = {"신규": k.iloc[idx]['신규'].item(), "누계" : k.loc[idx]['누계'].item()}
+        #     print("국가 : {}, 수량 : {}".format(k.iloc[idx]['구분.1'], k.iloc[idx]["신규"]))
+        data_set[dt.date(dt.now()).isoformat()] = data_sub_set
+
+        # Data insert to mongo
+        mg = to_mongo()
+        mg.insert_data(data_set, 'corona_data')
+
+if __name__ == "__main__":
+    crawler = Corona()
+    crawler.get_and_insert_data()
+    print("# Done")
